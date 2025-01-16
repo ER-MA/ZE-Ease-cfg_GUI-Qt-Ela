d7afc4f671f881c3f185403f8be827ab9cbf071c
Er Ma <erma@placeholder.com>
2025-01-16 15:01:32 +0800

    基于智能指针，重构通用 TreeItem 类；

--- EaseCFG/Pages/Keybind_TreeModel.h
+++ EaseCFG/Pages/Keybind_TreeModel.h
@@ -39,6 +39,31 @@ private:
     Univ_TreeItem* m_rootItem;
 
     Keybind_DB* _keybindDB; // 数据库对象
+
+    // 重写一次声明，以理清头绪
+    // 首先是通用的声明，暂不使用Univ_TreeItem*作为参数类型。
+    
+    // 返回子索引；传入所属父索引，及其所在的行号、列号
+    QModelIndex index(int row, int column, const QModelIndex& parent) const override;
+    // 返回父索引；传入子索引
+    QModelIndex parent(const QModelIndex& index) const override;
+    // 返回行数；传入父索引
+    int rowCount(const QModelIndex& parent) const override;
+    // 返回列数；传入父索引
+    int columnCount(const QModelIndex& parent) const override;
+    // 返回数据；传入索引、角色
+    QVariant data(const QModelIndex& index, int role) const override;
+
+    // 以上函数仅实现了数据的只读模式，数据编辑需要setData函数，增删数据项还需要其他函数。
+
+    // 从指定索引的row行处起，插入count行数据
+    bool insertRows(int row, int count, const QModelIndex& parent) override;
+    // 从指定索引的row行处开始，连续删除count行数据
+    bool removeRows(int row, int count, const QModelIndex& parent) override;
+    // 从指定索引的column列处起，插入count列数据
+    bool insertColumns(int column, int count, const QModelIndex& parent) override;
+    // 从指定索引的column列处开始，连续删除count列数据
+    bool removeColumns(int column, int count, const QModelIndex& parent) override;
 };
 
 #endif // KEYBIND_TREEMODEL_H
--- EaseCFG/Pages/Univ_TreeItem.h
+++ EaseCFG/Pages/Univ_TreeItem.h
@@ -12,20 +12,28 @@ public:
     explicit Univ_TreeItem(QVariantList data, Univ_TreeItem* parent = nullptr);
     ~Univ_TreeItem();
 
-    Univ_TreeItem *child(int number);
-    Univ_TreeItem *parent();
+    // 返回当前节点的第number个子节点
+    Univ_TreeItem* child(int number);
+    // 返回当前节点的父节点
+    Univ_TreeItem* parent();
 
+    // 返回当前节点的，子节点数量
     int childCount() const;
+    // 返回当前节点的，列数
     int columnCount() const;
+    // 返回当前节点中，第column列的数据
     QVariant data(int column) const;
+    // 设置当前节点中，第column列的数据
     bool setData(int column, const QVariant &value);
 
+    // 在position处，插入count个子节点，每行有columns列数据
     bool insertChildren(int position, int count, int columns);
+    // 在position处，插入columns列数据
     bool insertColumns(int position, int columns);
+    // 在position处，移除count个子节点
     bool removeChildren(int position, int count);
+    // 在position处，移除columns列数据
     bool removeColumns(int position, int columns);
-
-    bool appendChild(Univ_TreeItem *childItem);
 
     int row() const;
 
